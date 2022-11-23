d344b9acc9671f621e3e3b53af3b9a2886a340db
auraPasmin <aura.pasmin@gmail.com>
2022-11-22 19:59:20 -0500

    dictionary.js add file and modification index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuchosCustomHooks } from './componentes/MuchosCustomHooks';
+import { dictonario } from './componentes/dictonario'; 
 
 
 
@@ -8,3 +9,4 @@ const divRoot = document.querySelector('#root');
 
 
 ReactDOM.render( <MuchosCustomHooks/>, divRoot);
+ReactDOM.render( <dictonario/>);
